8714e647927dffc6c0760c295e9d39848b8451df
William Jacobsen <villi05.v.j@gmail.com>
2025-04-12 18:51:08 +0200

    small changes

--- ExtractFinancialsFromPDF/ExtractFinancialsFromPDF.py
+++ ExtractFinancialsFromPDF/ExtractFinancialsFromPDF.py
@@ -1,27 +1,27 @@
 import PyPDF2
 from predict import classify_text
 
-def ExtractFinancialsFromPDF(pdf_path):
-    total_lines_count = 0
-
+def ExtractFinancialsFromPDF(pdf_path, output_txt_path):  # This reduced the report size by 80%
     with open(pdf_path, "rb") as file:
         reader = PyPDF2.PdfReader(file)
-        for page in reader.pages:
-            text = page.extract_text()
-            if not text:
-                continue
-
-            for line in text.splitlines():
-                line = line.strip()
-                if not line:
+        with open(output_txt_path, 'w', encoding='utf-8') as txt_file:
+            for page in reader.pages:
+                text = page.extract_text()
+                if not text:
                     continue
 
-                if classify_text(line)[0] == "Numerical Data":
-                    print(line)
+                for line in text.splitlines():
+                    line = line.strip()
+                    if not line:
+                        continue
+                    
+                    if classify_text(line)[0] == "Numerical Data":
+                        txt_file.write(line + '\n')
 
 if __name__ == "__main__":
     import os
     os.system('cls')
 
     pdf_path = "../msft2021.pdf"
-    ExtractFinancialsFromPDF(pdf_path)
+    output_txt_path = "financials_output_unpolished.txt"
+    ExtractFinancialsFromPDF(pdf_path, output_txt_path)
